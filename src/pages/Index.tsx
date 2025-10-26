bfe2c33d524b515bc086a1ef5b3312ab359f6a4f
gabrielip19s <gabrielipsilva28@gmail.com>
2025-10-25 22:54:54 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -129,7 +129,7 @@ const Index = () => {
         {[
   {
     title: "Design de Sobrancelhas",
-    description: "Modelagem perfeita que realça sua beleza natural",
+    description: "Técnica que modela as sobrancelhas de acordo com o formato do rosto, realçando a expressão natural e deixando o olhar mais harmonioso e definido",
     price: "R$ 70",
     image: serviceDesign,
   },
