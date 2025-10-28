1096221ea4d2b1cedc5b3e517d3ceeec7382cdcd
Julia <120433417+liaJu-0@users.noreply.github.com>
2025-10-28 11:12:01 -0300

    Redirecionamento do usuário não logado para a tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Servicos from "./pages/Servicos";
-//import Portfolio from "./pages/Portfolio";
+import Portfolio from "./pages/Portfolio";
 import Contato from "./pages/Contato";
 import Agendamento from "./pages/Agendamento";
 import Auth from "./pages/Auth";
@@ -14,7 +14,7 @@ import AdminAgendamentos from "./pages/AdminAgendamentos";
 import AdminSettings from "./pages/AdminSettings";
 import NotFound from "./pages/NotFound";
 import ResetPassword from "./pages/ResetPassword";
-import ProtectedRoute from "@/components/ProtectedRoute";
+import ProtectedRoute from "@/components/ui/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
